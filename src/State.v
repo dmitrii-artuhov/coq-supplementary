7188e37790524bbce43b40fb9c055f5ac1d76898
Dmitrii Artiukhov <artuhovdmitrii@gmail.com>
2025-03-23 23:17:41 +0000

    Complete State.v file

--- src/State.v
+++ src/State.v
@@ -5,6 +5,7 @@ Import ListNotations.
 Require Import Lia.
 Require Export Arith Arith.EqNat.
 Require Export Id.
+Require Import Coq.Program.Equality.
 
 Section S.
 
@@ -47,49 +48,113 @@ Section S.
     (SN : st / x => n)
     (SM : st / x => m) :
     n = m. 
-  Proof. admit. Admitted.
-  
+  Proof.
+    intros.
+    induction SN.
+      - inversion SM. subst. reflexivity. subst. contradiction.
+      - inversion SM.
+        + subst. contradiction.
+        + subst. apply IHSN. assumption.
+  Qed.
+    
   Lemma update_eq (st : state) (x : id) (n : A) :
     st [x <- n] / x => n.
-  Proof. admit. Admitted.
+  Proof.
+    intros.
+    unfold update.
+    apply st_binds_hd.
+  Qed.
 
   Lemma update_neq (st : state) (x2 x1 : id) (n m : A)
         (NEQ : x2 <> x1) : st / x1 => m <-> st [x2 <- n] / x1 => m.
-  Proof. admit. Admitted.
+  Proof.
+    intros. split.
+    - intros. unfold update. apply st_binds_tl.
+      + symmetry. assumption.
+      + assumption.
+    - intros. unfold update in H. inversion H. subst.
+      + contradiction.
+      + subst. assumption.
+  Qed.  
   
   Lemma update_shadow (st : state) (x1 x2 : id) (n1 n2 m : A) :
     st[x2 <- n1][x2 <- n2] / x1 => m <-> st[x2 <- n2] / x1 => m.
-  Proof. admit. Admitted.
-  
+  Proof.
+    split.
+    - intros.
+      (* dependent destruction H. *)
+      inversion H. subst.
+        + unfold update in *. apply st_binds_hd.
+        + subst. unfold update in *. apply st_binds_tl.
+          * assumption.
+          * inversion H6. subst.
+            { contradiction. }
+            { subst. assumption. }
+    - intros.
+      inversion H. subst.
+        + constructor.
+        + subst. unfold update in *. apply st_binds_tl.
+          * assumption.
+          * apply st_binds_tl. assumption. assumption.
+  Qed.
+
   Lemma update_same (st : state) (x1 x2 : id) (n1 m : A)
         (SN : st / x1 => n1)
         (SM : st / x2 => m) :
     st [x1 <- n1] / x2 => m.
-  Proof. admit. Admitted.
-  
+  Proof.
+    destruct (id_eq_dec x1 x2).
+      - subst. remember (state_deterministic st x2 n1 m). rewrite <- e. apply update_eq.
+        + assumption.
+        + assumption.
+      - apply update_neq.
+        + assumption.
+        + assumption.
+  Qed.
+
   Lemma update_permute (st : state) (x1 x2 x3 : id) (n1 n2 m : A)
         (NEQ : x2 <> x1)
         (SM : st [x2 <- n1][x1 <- n2] / x3 => m) :
     st [x1 <- n2][x2 <- n1] / x3 => m.
-  Proof. admit. Admitted.
+  Proof.
+    intros. unfold update in *.
+    dependent destruction SM. subst. 
+      - apply st_binds_tl.
+        + symmetry. assumption.
+        + apply st_binds_hd.
+      - dependent destruction SM.
+        + constructor.
+        + apply st_binds_tl. assumption. apply st_binds_tl. assumption. assumption.
+  Qed.
 
   Lemma state_extensional_equivalence (st st' : state) (H: forall x z, st / x => z <-> st' / x => z) : st = st'.
-  Proof. admit. Admitted.
+  Proof. Abort.
 
   Definition state_equivalence (st st' : state) := forall x a, st / x => a <-> st' / x => a.
 
   Notation "st1 ~~ st2" := (state_equivalence st1 st2) (at level 0).
 
   Lemma st_equiv_refl (st: state) : st ~~ st.
-  Proof. admit. Admitted.
+  Proof.
+    unfold state_equivalence. intros. reflexivity.
+  Qed.
 
   Lemma st_equiv_symm (st st': state) (H: st ~~ st') : st' ~~ st.
-  Proof. admit. Admitted.
+  Proof.
+    unfold state_equivalence. intros. symmetry. apply H.
+  Qed.
 
   Lemma st_equiv_trans (st st' st'': state) (H1: st ~~ st') (H2: st' ~~ st'') : st ~~ st''.
-  Proof. admit. Admitted.
+  Proof.
+    unfold state_equivalence. intros. split.
+      - intros. apply H2. apply H1. assumption.
+      - intros. apply H1. apply H2. assumption.
+  Qed.
 
   Lemma equal_states_equive (st st' : state) (HE: st = st') : st ~~ st'.
-  Proof. admit. Admitted.
-  
+  Proof.
+    unfold state_equivalence. intros. split.
+      - intros. rewrite <- HE. assumption.
+      - intros. rewrite HE. assumption.
+  Qed.
 End S.
